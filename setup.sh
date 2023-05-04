66581115ca906172e991cde1ec41688ef8f0d291
Ana <mihalceanuana@gmail.com>
2023-05-04 17:46:56 +0200

    Fix typos in scripts

--- setup.sh
+++ setup.sh
@@ -1,37 +1,36 @@
-read -p "Enter your name : " name
-echo "Hi, $name. Let us be friends!"
-
-read -p "Please provide your OCI region: "  -n 1 -r
+read -p "Please provide your OCI region: "  -r
 echo    # (optional) move to a new line
-if [[  -z  "$REPLY" ]]
+if [[   ! -z "$REPLY" ]]
 then
+echo $REPLY
 pulumi config set oci:region $REPLY
 fi
 
-read -p "Please provide your tenancy OCID: "  -n 1 -r
+read -p "Please provide your tenancy OCID: " -r
 echo    # (optional) move to a new line
-if [[  -z  "$REPLY" ]]
+if [[   ! -z "$REPLY" ]]
 then
+echo $REPLY
 pulumi config set oci:tenancyOcid $REPLY --secret
 fi
 
-read -p "Please provide your userOcid: "  -n 1 -r
+read -p "Please provide your userOcid: " -r
 echo    # (optional) move to a new line
-if [[  -z  "$REPLY" ]]
+if [[   ! -z "$REPLY" ]]
 then
-pulumi config set oci:userOcid  --secret
+pulumi config set oci:userOcid $REPLY --secret
 fi
 
-read -p "Please provide your oci fingerprint: "  -n 1 -r
+read -p "Please provide your oci fingerprint: " -r
 echo    # (optional) move to a new line
-if [[  -z  "$REPLY" ]]
+if [[   ! -z "$REPLY" ]]
 then
 pulumi config set oci:fingerprint $REPLY --secret
 fi
 
-read -p "Please provide parent compartment ocid: "  -n 1 -r
+read -p "Please provide parent compartment ocid: " -r
 echo    # (optional) move to a new line
-if [[  -z  "$REPLY" ]]
+if [[   ! -z "$REPLY" ]]
 then
 pulumi config set compartment_ocid $REPLY
 fi
@@ -47,7 +46,7 @@ pulumi preview
 pulumi up
 read -p "Please provide the number of compute instances to create: " -n 1 -r
 echo    # (optional) move to a new line
-if [[  -z  "$REPLY" ]]
+if [[   ! -z "$REPLY" ]]
 then
 pulumi config set amount_vm $REPLY
 fi
